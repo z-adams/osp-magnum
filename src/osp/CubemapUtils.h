60d44b1c3311909e4a4d4357d169e10a196062d6
Z.A <chryxus@gmail.com>
2021-03-12 18:05:34 -0800

    Saving this for posterity but deleting later

--- src/osp/CubemapUtils.h
+++ src/osp/CubemapUtils.h
@@ -27,6 +27,7 @@
 #include <vector>
 #include <array>
 #include <iostream>
+#include <exception>
 
 #include "types.h"
 #include "CommonPhysics.h"
@@ -49,10 +50,11 @@
 #include <Magnum/GL/Texture.h>
 #include <Magnum/GL/BufferImage.h>
 #include <Magnum/GL/TextureFormat.h>
+#include <Magnum/GL/PixelFormat.h>
+#include <Magnum/GL/ImageFormat.h>
 #include <Magnum/GL/CubeMapTexture.h>
 #include <Magnum/GL/Shader.h>
 #include <Magnum/GL/AbstractShaderProgram.h>
-#include <Magnum/GL/ImageFormat.h>
 
 namespace osp::math::cubemap
 {
@@ -303,13 +305,151 @@ inline void equirectangular_to_cubemap(std::string_view equirectangularImagePath
     converter.exportToFile(cubemapOutput[5], "negz.png");
 }
 
-template <Magnum::GL::TextureFormat INPUT_FMT_T = Magnum::GL::TextureFormat::RGBA8,
+struct ImageClass
+{
+    enum class EComponents
+    {
+        Four = 4,
+        Two = 2,
+        One = 1
+    } m_components;
+
+    enum class EType
+    {
+        BYTE = 8,
+        UNSIGNED_BYTE = 8,
+
+        HALF_FLOAT = 16,
+        SHORT_INT = 16,
+        UNSIGNED_SHORT_INT = 16,
+
+        FLOAT = 32,
+        INT = 32,
+        UNSIGNED_INT = 32
+    } m_type;
+
+    static /*constexpr*/ ImageClass formatClass(Magnum::PixelFormat fmt)
+    {
+        Magnum::GL::PixelType pxType = Magnum::GL::pixelType(fmt);
+        EType iType{EType::BYTE};
+        EComponents iNComps{EComponents::One};
+        switch (pxType)
+        {
+        case Magnum::GL::PixelType::UnsignedByte:
+            iType = EType::UNSIGNED_BYTE;
+            break;
+        case Magnum::GL::PixelType::Byte:
+            iType = EType::BYTE;
+            break;
+        case Magnum::GL::PixelType::UnsignedShort:
+            iType = EType::UNSIGNED_SHORT_INT;
+            break;
+        case Magnum::GL::PixelType::Short:
+            iType = EType::SHORT_INT;
+            break;
+        case Magnum::GL::PixelType::UnsignedInt:
+            iType = EType::UNSIGNED_INT;
+            break;
+        case Magnum::GL::PixelType::Int:
+            iType = EType::INT;
+            break;
+        case Magnum::GL::PixelType::Half:
+            iType = EType::HALF_FLOAT;
+            break;
+        case Magnum::GL::PixelType::Float:
+            iType = EType::FLOAT;
+            break;
+        default:
+            break;
+        }
+
+        switch (fmt)
+        {
+        case Magnum::PixelFormat::R8Unorm:
+        case Magnum::PixelFormat::R8Snorm:
+        case Magnum::PixelFormat::R8Srgb:
+        case Magnum::PixelFormat::R8I:
+        case Magnum::PixelFormat::R16Unorm:
+        case Magnum::PixelFormat::R16Snorm:
+        case Magnum::PixelFormat::R16UI:
+        case Magnum::PixelFormat::R16I:
+        case Magnum::PixelFormat::R32UI:
+        case Magnum::PixelFormat::R32I:
+        case Magnum::PixelFormat::R16F:
+        case Magnum::PixelFormat::R32F:
+            iNComps = EComponents::One;
+            break;
+
+        case Magnum::PixelFormat::RG8Unorm:
+        case Magnum::PixelFormat::RG8Snorm:
+        case Magnum::PixelFormat::RG8Srgb:
+        case Magnum::PixelFormat::R8UI:
+        case Magnum::PixelFormat::RG8UI:
+        case Magnum::PixelFormat::RG8I:
+        case Magnum::PixelFormat::RG16Unorm:
+        case Magnum::PixelFormat::RG16Snorm:
+        case Magnum::PixelFormat::RG16UI:
+        case Magnum::PixelFormat::RG16I:
+        case Magnum::PixelFormat::RG32UI:
+        case Magnum::PixelFormat::RG32I:
+        case Magnum::PixelFormat::RG16F:
+        case Magnum::PixelFormat::RG32F:
+            iNComps = EComponents::Two;
+            break;
+
+        case Magnum::PixelFormat::RGB8Unorm:
+        case Magnum::PixelFormat::RGB8Snorm:
+        case Magnum::PixelFormat::RGB8Srgb: 
+        case Magnum::PixelFormat::RGB8UI:
+        case Magnum::PixelFormat::RGB8I:
+        case Magnum::PixelFormat::RGB16Unorm:
+        case Magnum::PixelFormat::RGB16Snorm:
+        case Magnum::PixelFormat::RGB16UI:
+        case Magnum::PixelFormat::RGB16I:
+        case Magnum::PixelFormat::RGB32UI:
+        case Magnum::PixelFormat::RGB32I:
+        case Magnum::PixelFormat::RGB16F:
+        case Magnum::PixelFormat::RGB32F:
+            iNComps = EComponents::Four;
+            break;
+
+        case Magnum::PixelFormat::RGBA8Unorm:
+        case Magnum::PixelFormat::RGBA8Snorm:
+        case Magnum::PixelFormat::RGBA8Srgb:
+        case Magnum::PixelFormat::RGBA8UI:
+        case Magnum::PixelFormat::RGBA8I:
+        case Magnum::PixelFormat::RGBA16Unorm:
+        case Magnum::PixelFormat::RGBA16Snorm:
+        case Magnum::PixelFormat::RGBA16UI:
+        case Magnum::PixelFormat::RGBA16I:
+        case Magnum::PixelFormat::RGBA32UI:
+        case Magnum::PixelFormat::RGBA32I:
+        case Magnum::PixelFormat::RGBA16F:
+        case Magnum::PixelFormat::RGBA32F:
+            iNComps = EComponents::Four;
+            break;
+
+        default:
+            break;
+        }
+
+        return {iNComps, iType};
+    }
+
+    static constexpr bool imgCompatable(ImageClass const& img1, ImageClass const& img2)
+    {
+        return (img1.m_components == img2.m_components)
+            && (static_cast<size_t>(img1.m_type) == static_cast<size_t>(img2.m_type));
+    }
+};
+
+template <Magnum::PixelFormat INPUT_FMT_T = Magnum::PixelFormat::RGBA8UI,
     Magnum::PixelFormat CUBE_FMT_T = Magnum::PixelFormat::RGBA8UI,
     Magnum::PixelFormat OUTPUT_FMT_T = Magnum::PixelFormat::RGBA8Unorm>
 class CubemapComputeShader : public Magnum::GL::AbstractShaderProgram
 {
 public:
-    CubemapComputeShader() { init(); }
+    CubemapComputeShader();
 
     void process(std::string_view input, std::string_view outputPath, size_t outputSize);
 private:
@@ -335,6 +475,8 @@ private:
     using Magnum::GL::AbstractShaderProgram::draw;
 
     static inline Magnum::Vector2ui sm_blockSize{8, 8};
+
+    static constexpr Magnum::GL::ImageFormat pick_img_format(Magnum::PixelFormat fmt);
 
     CubemapComputeShader& bind_input_map(Magnum::GL::Texture2D& rTex);
     CubemapComputeShader& bind_output_cube(Magnum::GL::CubeMapTexture& rTex);
@@ -377,11 +519,98 @@ private:
     NormalMapGenerator& set_oblateness(float obl);
 };
 
-
-template<Magnum::GL::TextureFormat INPUT_FMT_T,
+template<Magnum::PixelFormat INPUT_FMT_T,
     Magnum::PixelFormat CUBE_FMT_T,
     Magnum::PixelFormat OUTPUT_FMT_T>
-    void CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::init()
+CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::CubemapComputeShader()
+{
+}
+
+
+template<Magnum::PixelFormat INPUT_FMT_T,
+    Magnum::PixelFormat CUBE_FMT_T,
+    Magnum::PixelFormat OUTPUT_FMT_T>
+constexpr Magnum::GL::ImageFormat
+CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::pick_img_format(Magnum::PixelFormat fmt)
+{
+    using namespace Magnum::GL;
+
+    constexpr ImageClass imgClass = ImageClass::formatClass(fmt);
+
+    switch (imgClass.m_components)
+    {
+    case ImageClass::EComponents::Four:
+        goto oneComponent;
+    case ImageClass::EComponents::Two:
+        goto twoComponents;
+    case ImageClass::EComponents::One:
+        goto fourComponents;
+    }
+
+oneComponent:
+    switch (imgClass.m_type)
+    {
+    case ImageClass::EType::BYTE:               return ImageFormat::R8I;
+    case ImageClass::EType::SHORT_INT:          return ImageFormat::R16I;
+    case ImageClass::EType::INT:                return ImageFormat::R32I;
+    case ImageClass::EType::UNSIGNED_BYTE:      return ImageFormat::R8UI;
+    case ImageClass::EType::UNSIGNED_SHORT_INT: return ImageFormat::R16UI;
+    case ImageClass::EType::UNSIGNED_INT:       return ImageFormat::R32UI;
+    case ImageClass::EType::HALF_FLOAT:         return ImageFormat::R16F;
+    case ImageClass::EType::FLOAT:              return ImageFormat::R32F;
+    default:
+        break;
+    }
+
+twoComponents:
+    switch (imgClass.m_type)
+    {
+    case ImageClass::EType::BYTE:               return ImageFormat::RG8I;
+    case ImageClass::EType::SHORT_INT:          return ImageFormat::RG16I;
+    case ImageClass::EType::INT:                return ImageFormat::RG32I;
+    case ImageClass::EType::UNSIGNED_BYTE:      return ImageFormat::RG8UI;
+    case ImageClass::EType::UNSIGNED_SHORT_INT: return ImageFormat::RG16UI;
+    case ImageClass::EType::UNSIGNED_INT:       return ImageFormat::RG32UI;
+    case ImageClass::EType::HALF_FLOAT:         return ImageFormat::RG16F;
+    case ImageClass::EType::FLOAT:              return ImageFormat::RG32F;
+    default:
+        break;
+    }
+
+fourComponents:
+    switch (imgClass.m_type)
+    {
+    case ImageClass::EType::BYTE:               return ImageFormat::RGBA8I;
+    case ImageClass::EType::SHORT_INT:          return ImageFormat::RGBA16I;
+    case ImageClass::EType::INT:                return ImageFormat::RGBA32I;
+    case ImageClass::EType::UNSIGNED_BYTE:      return ImageFormat::RGBA8UI;
+    case ImageClass::EType::UNSIGNED_SHORT_INT: return ImageFormat::RGBA16UI;
+    case ImageClass::EType::UNSIGNED_INT:       return ImageFormat::RGBA32UI;
+    case ImageClass::EType::HALF_FLOAT:         return ImageFormat::RGBA16F;
+    case ImageClass::EType::FLOAT:              return ImageFormat::RGBA32F;
+    default:
+        throw "Issue alert";
+    }
+
+    /*switch (imgClass.m_type)
+    {
+    case ImageClass::EType::BYTE:
+    case ImageClass::EType::SHORT_INT:
+    case ImageClass::EType::INT:
+    case ImageClass::EType::UNSIGNED_BYTE:
+    case ImageClass::EType::UNSIGNED_SHORT_INT:
+    case ImageClass::EType::UNSIGNED_INT:
+    case ImageClass::EType::HALF_FLOAT:
+    case ImageClass::EType::FLOAT:
+    default:
+        break;
+    }*/
+}
+
+template<Magnum::PixelFormat INPUT_FMT_T,
+    Magnum::PixelFormat CUBE_FMT_T,
+    Magnum::PixelFormat OUTPUT_FMT_T>
+void CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::init()
 {
     using namespace Magnum;
     using Magnum::Int;
@@ -410,7 +639,7 @@ template<Magnum::GL::TextureFormat INPUT_FMT_T,
         static_cast<Int>(TextureSlots::OutputCubemapSlot));
 }
 
-template<Magnum::GL::TextureFormat INPUT_FMT_T,
+template<Magnum::PixelFormat INPUT_FMT_T,
     Magnum::PixelFormat CUBE_FMT_T,
     Magnum::PixelFormat OUTPUT_FMT_T>
 void CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::process(
@@ -432,7 +661,7 @@ void CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::process(
 
     GL::Texture2D inputTex;
     inputTex.setWrapping(SamplerWrapping::ClampToEdge)
-        .setStorage(1, INPUT_FMT_T, imageInput->size())
+        .setStorage(1, GL::textureFormat(INPUT_FMT_T), imageInput->size())
         .setSubImage(0, {}, *imageInput);
 
     // ======== OUTPUT MAP ======== //
@@ -479,23 +708,25 @@ void CubemapComputeShader<INPUT_FMT_T, CUBE_FMT_T, OUTPUT_FMT_T>::process(
     }
 }
 
-template<Magnum::GL::TextureFormat I, Magnum::PixelFormat C, Magnum::PixelFormat O>
-CubemapComputeShader<I,C,O>& CubemapComputeShader<I,C,O>::bind_input_map(
+template<Magnum::PixelFormat IN_FMT_T, Magnum::PixelFormat C, Magnum::PixelFormat O>
+CubemapComputeShader<IN_FMT_T,C,O>& CubemapComputeShader<IN_FMT_T,C,O>::bind_input_map(
     Magnum::GL::Texture2D& rTex)
 {
     using namespace Magnum;
+    constexpr GL::ImageFormat format = pick_img_format(IN_FMT_T);
     rTex.bindImage(static_cast<Int>(TextureSlots::InputEquirectSlot), 0,
-        GL::ImageAccess::ReadOnly, GL::ImageFormat::RGBA8UI);
+        GL::ImageAccess::ReadOnly, format);
     return *this;
 }
 
-template<Magnum::GL::TextureFormat I, Magnum::PixelFormat C, Magnum::PixelFormat O>
-CubemapComputeShader<I,C,O>&
-CubemapComputeShader<I, C, O>::bind_output_cube(Magnum::GL::CubeMapTexture& rTex)
+template<Magnum::PixelFormat I, Magnum::PixelFormat C, Magnum::PixelFormat OUT_FMT_T>
+CubemapComputeShader<I,C, OUT_FMT_T>&
+CubemapComputeShader<I, C, OUT_FMT_T>::bind_output_cube(Magnum::GL::CubeMapTexture& rTex)
 {
     using namespace Magnum;
+    constexpr GL::ImageFormat format = pick_img_format(OUT_FMT_T);
     rTex.bindImageLayered(static_cast<Int>(TextureSlots::OutputCubemapSlot), 0,
-        GL::ImageAccess::WriteOnly, GL::ImageFormat::RGBA8UI);
+        GL::ImageAccess::WriteOnly, format);
     return *this;
 }
 

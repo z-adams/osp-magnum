065ae44f632871193a25c076395014c2be26fdc3
Z.A <chryxus@gmail.com>
2021-03-24 22:59:10 -0700

    RT beginnings

---
+++ src/osp/Shaders/RT.cpp
@@ -0,0 +1,216 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "RT.h"
+
+#include <Magnum/GL/Shader.h>
+#include <Magnum/GL/Version.h>
+#include <Magnum/GL/ImageFormat.h>
+#include <Corrade/Containers/ArrayViewStl.h>
+#include <Magnum/Trade/MeshData.h>
+
+#include <osp/Active/SysDebugRender.h>
+#include <adera/Shaders/PlanetShader.h>
+
+using namespace osp::active::shader;
+using namespace Magnum;
+
+void RTShader::init()
+{
+    GL::Shader prog{GL::Version::GL430, GL::Shader::Type::Compute};
+    prog.addFile("OSPData/adera/Shaders/RT.comp");
+
+    CORRADE_INTERNAL_ASSERT_OUTPUT(prog.compile());
+    attachShader(prog);
+    CORRADE_INTERNAL_ASSERT_OUTPUT(link());
+
+    setUniform(
+        static_cast<Int>(UniformPos::OutputImg),
+        static_cast<Int>(ImageSlots::OutputMap));
+}
+
+RTShader& RTShader::bind_objects_list(GL::Buffer& objects)
+{
+    objects.bind(GL::Buffer::Target::Uniform, static_cast<Int>(UniformPos::ObjectBlock));
+    return *this;
+}
+
+RTShader& RTShader::bind_light_list(GL::Buffer& lights)
+{
+    lights.bind(GL::Buffer::Target::Uniform, static_cast<Int>(UniformPos::LightBlock));
+    return *this;
+}
+
+RTShader& RTShader::bind_output_img(GL::Texture2D& rTex)
+{
+    rTex.bindImage(static_cast<Int>(ImageSlots::OutputMap), 0,
+        GL::ImageAccess::WriteOnly, GL::ImageFormat::R8);
+    return *this;
+}
+
+RTShader& RTShader::bind_gbuffer(GL::Buffer& rGBuf)
+{
+    rGBuf.bind(GL::Buffer::Target::ShaderStorage, static_cast<Int>(UniformPos::GBuffer));
+    return *this;
+}
+
+RTShader& RTShader::bind_triangle_buffer(GL::Buffer& rTriangles)
+{
+    rTriangles.bind(GL::Buffer::Target::ShaderStorage, static_cast<Int>(UniformPos::TriBuffer));
+    return *this;
+}
+
+void RTShader::raytrace(ActiveScene& rScene, ACompCamera const& camera,
+    GL::Buffer& gBuffer, GL::Texture2D& target)
+{
+    std::vector<ObjectData> objects;
+    std::vector<DirectionalLight> lights = {DirectionalLight{{1.0f, 0.0f, 0.0f}, 1.0f}};
+    std::vector<Triangle> tris;
+
+    using adera::shader::PlanetShader;
+    auto& pkg = rScene.get_application().debug_find_package("lzdb");
+    auto geometry = rScene.get_registry().view<CompDrawableDebug, ACompTransform>(
+        entt::exclude<PlanetShader, CompBackgroundDebug,
+        CompOverlayDebug, CompTransparentDebug>);
+    for (auto [e, drawable, transform] : geometry.each())
+    {
+        DependRes<Trade::MeshData> mesh = pkg.get<Trade::MeshData>(drawable.m_mesh.name());
+
+        size_t firstTriIndex = tris.size();
+
+        if (mesh->isIndexed())
+        {
+            auto indices = Corrade::Containers::arrayCast<UnsignedInt>(mesh->indexData());
+            auto vertices = Corrade::Containers::arrayCast<Vector3>(mesh->vertexData());
+
+            for (size_t i = 0; i < indices.size(); i += 3)
+            {
+                tris.emplace_back(
+                    vertices[indices[i + 0]],
+                    vertices[indices[i + 1]],
+                    vertices[indices[i + 2]]
+                );
+            }
+        }
+        else
+        {
+            auto vertices = Corrade::Containers::arrayCast<Vector3>(mesh->vertexData());
+            for (size_t i = 0; i < vertices.size(); i += 3)
+            {
+                tris.emplace_back(
+                    vertices[i + 0],
+                    vertices[i + 1],
+                    vertices[i + 2]
+                );
+            }
+        }
+
+        size_t numTriangles = tris.size() - firstTriIndex;
+
+        Box boundingBox; // get from somewhere
+        Box aabb = box_to_AABB(boundingBox, Matrix3{transform.m_transformWorld});
+        aabb.translate(transform.m_transformWorld.translation());
+
+        ObjectData obj
+        {
+            aabb.m_max,
+            firstTriIndex,
+            aabb.m_min,
+            numTriangles
+        };
+    }
+
+    if (m_objectBuffer.id() == 0)
+    {
+        m_objectBuffer.setData(objects);
+    }
+    else
+    {
+        m_objectBuffer.setSubData(0, objects);
+    }
+
+    if (m_lightBuffer.id() == 0)
+    {
+        m_lightBuffer.setData(lights);
+    }
+    /*else
+    {
+        m_objectBuffer.setSubData(0, lights);
+    }*/
+
+    if (m_triangleBuffer.id() == 0)
+    {
+        m_triangleBuffer.setData(tris);
+    }
+    /*else
+    {
+        m_objectBuffer.setSubData(0, tris);
+    }*/
+
+    bind_objects_list(m_objectBuffer);
+    bind_light_list(m_lightBuffer);
+    bind_output_img(target);
+    bind_gbuffer(gBuffer);
+    bind_triangle_buffer(m_triangleBuffer);
+
+    constexpr Vector3ui dimensions{1280, 720, 0};
+    dispatchCompute(dimensions);
+}
+
+Box box_to_AABB(Box& box, Magnum::Matrix3 const& rot)
+{
+    Box output{{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            float a = rot[i][j] * box.m_min[j];
+            float b = rot[i][j] * box.m_max[j];
+            output.m_min[i] += (a < b) ? a : b;
+            output.m_max[i] += (a < b) ? b : a;
+        }
+    }
+
+    return output;
+}
+
+using namespace osp::active;
+using namespace std::placeholders;
+
+SysRaytracer::SysRaytracer(ActiveScene& rScene)
+    : m_scene(rScene)
+    , m_order(rScene.get_render_order(), "RT", "debug", "",
+        std::bind(&raytrace, this, _1))
+{}
+
+void SysRaytracer::raytrace(ACompCamera const& camera)
+{
+    auto& rScene = m_scene;
+    auto& gfxRes = rScene.get_context_resources();
+    DependRes<RTShader> shader = gfxRes.get<RTShader>("RT_shader");
+    DependRes<GL::Buffer> gbuf = gfxRes.get<GL::Buffer>("gbuffer");
+    DependRes<GL::Texture2D> color = gfxRes.get<GL::Texture2D>("output");
+    shader->raytrace(rScene, camera, *gbuf, *color);
+}
---
+++ src/osp/Shaders/RT.h
@@ -0,0 +1,164 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+#include <Magnum/GL/AbstractShaderProgram.h>
+#include <Magnum/GL/Framebuffer.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/GL/Buffer.h>
+
+#include <osp/Active/ActiveScene.h>
+#include <osp/Resource/Resource.h>
+
+namespace osp::active::shader
+{
+
+#pragma pack(push, 1)
+struct ObjectData
+{
+    Magnum::Vector3 m_AABBMaxs;
+    Magnum::UnsignedInt m_firstTriIndex;
+    Magnum::Vector3 m_AABBMins;
+    Magnum::UnsignedInt m_numTriangles;
+};
+#pragma pack(pop)
+
+struct DirectionalLight
+{
+    Magnum::Vector3 m_direction;
+    Magnum::Float m_brightness;
+};
+
+#pragma pack(push, 1)
+struct GBufferPixel
+{
+    Magnum::Vector3 m_castRay;
+    Magnum::Float m_sampleDepth;
+    Magnum::Vector2 m_normalXY;
+    Magnum::Vector2 m_hitUV;
+};
+#pragma pack(pop)
+
+struct Triangle
+{
+    Magnum::Vector3 m_v0;
+    Magnum::Vector3 m_v1;
+    Magnum::Vector3 m_v2;
+
+    Triangle(Magnum::Vector3 v0, Magnum::Vector3 v1, Magnum::Vector3 v2)
+        : m_v0(std::move(v0)), m_v1(std::move(v1)), m_v2(std::move(v2))
+    {}
+};
+
+struct Box
+{
+    Magnum::Vector3 m_min;
+    Magnum::Vector3 m_max;
+
+    void translate(Magnum::Vector3 r)
+    {
+        m_min += r;
+        m_max += r;
+    }
+};
+
+// Jim Arvo, Graphics Gems (1990)
+Box box_to_AABB(Box& box, Magnum::Matrix3 const& rot);
+
+class RTShader : public Magnum::GL::AbstractShaderProgram
+{
+public:
+    RTShader() { init(); }
+
+    void raytrace(ActiveScene& rScene, ACompCamera const& camera,
+        Magnum::GL::Buffer& gBuffer, Magnum::GL::Texture2D& target);
+
+private:
+    void init();
+
+    // Uniforms
+    enum class UniformPos : Magnum::Int
+    {
+        ObjectBlock = 0,
+        LightBlock = 1,
+        OutputImg = 2,
+        GBuffer = 3,
+        TriBuffer = 4
+    };
+
+    // Buffer bindings
+    enum class BufferPos : Magnum::Int
+    {
+        ObjectsBuf = 0,
+        LightsBuf = 1,
+        GBuf = 3,
+        TriBuf = 4
+    };
+
+    // ImageSlots
+    enum class ImageSlots : Magnum::Int
+    {
+        OutputMap = 0
+    };
+
+    // Hide irrelevant calls
+    using Magnum::GL::AbstractShaderProgram::drawTransformFeedback;
+    using Magnum::GL::AbstractShaderProgram::draw;
+
+    // Uniform bindings
+    RTShader& bind_objects_list(Magnum::GL::Buffer& objects);
+    RTShader& bind_light_list(Magnum::GL::Buffer& lights);
+    RTShader& bind_output_img(Magnum::GL::Texture2D& rTex);
+    RTShader& bind_gbuffer(Magnum::GL::Buffer& rGBuf);
+    RTShader& bind_triangle_buffer(Magnum::GL::Buffer& rTriangles);
+
+    // Data buffers (TMP)
+    Magnum::GL::Buffer m_objectBuffer{NoCreate};
+    Magnum::GL::Buffer m_lightBuffer{NoCreate};
+    Magnum::GL::Buffer m_gBuffer{NoCreate};
+    Magnum::GL::Buffer m_triangleBuffer{NoCreate};
+};
+
+}
+
+namespace osp::active
+{
+
+class SysRaytracer : public osp::active::IDynamicSystem
+{
+public:
+    static inline std::string smc_name = "Raytracer";
+
+    SysRaytracer(ActiveScene& rScene);
+    ~SysRaytracer() = default;
+
+    void raytrace(ACompCamera const& camera);
+
+private:
+    ActiveScene& m_scene;
+    RenderOrderHandle_t m_order;
+};
+
+}

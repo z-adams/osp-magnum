61d024666e67e3195b41fd09b252b2d95d6993ac
Z.A <chryxus@gmail.com>
2021-04-21 17:39:25 -0700

    Variable-length paths (untested)

--- src/osp/Active/SysRender.cpp
+++ src/osp/Active/SysRender.cpp
@@ -172,11 +172,6 @@ osp::active::ActiveEnt SysRender::get_default_rendertarget(ActiveScene& rScene)
     return rScene.hier_get_root();
 }
 
-ActiveEnt SysRender::get_default_rendertarget(ActiveScene& rScene)
-{
-    return rScene.hier_get_root();
-}
-
 void SysRender::display_default_rendertarget(ActiveScene& rScene)
 {
     using Magnum::GL::Renderer;
